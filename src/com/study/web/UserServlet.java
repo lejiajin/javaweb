c910fce3fac66157bfe4a55ef2f6b2170d860f4f
Administrator <1185272145@qq.com>
2020-11-24 09:25:45 +0800

    study

--- src/com/study/web/UserServlet.java
+++ src/com/study/web/UserServlet.java
@@ -73,7 +73,6 @@ public class UserServlet extends HttpServlet {
             Method method=this.getClass().getDeclaredMethod(action,HttpServletRequest.class,HttpServletResponse.class);
             try {
                 method.invoke(this, req, resp);
-                System.out.println(123);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
